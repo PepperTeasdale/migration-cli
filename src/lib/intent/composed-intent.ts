375d030f16c9d9f9ad1642ffc8d037f8318d9277
Hervé Labas <herve@contentful.com>
2018-02-09 18:34:38 +0100

    fixup! Updated after review

--- src/lib/intent/composed-intent.ts
+++ src/lib/intent/composed-intent.ts
@@ -2,7 +2,6 @@ import Intent from '../interfaces/intent'
 import { RawStep } from '../interfaces/raw-step'
 import { PlanMessage, Section } from '../interfaces/plan-message'
 import { difference, groupBy, flatten, values, entries } from 'lodash'
-import { APIAction } from '../action/action';
 
 function mergeSections (sections: Section[]): Section {
   const sameSections = groupBy(sections, 'heading')
